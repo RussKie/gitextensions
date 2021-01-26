bdd87c314db8c3c53ad24ba40218b113b20e4dcc
RussKie <russkie@gmail.com>
2021-01-26 22:26:23 +1100

    .

--- GitExtUtils/ArrayExtensions.cs
+++ GitExtUtils/ArrayExtensions.cs
@@ -14,7 +14,7 @@ namespace GitCommands
         }
 
         [MustUseReturnValue]
-        public static T[] Append<T>([NotNull] this T[] array, T element)
+        public static T[] AppendTo<T>([NotNull] this T[] array, T element)
         {
             var larger = new T[array.Length + 1];
             Array.Copy(array, larger, array.Length);
