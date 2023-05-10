121c7e109f9901b8753b0754618b6092ed69287f
RussKie <russkie@gmail.com>
2023-05-10 22:03:44 +1000

    not working

---
+++ Setup/build.cmd
@@ -0,0 +1,1 @@
+dotnet wix build Product.wxs -d Version=34.0.0.0 -d NumericVersion:34.0.0.0 -d ArtifactsBinPath=D:\Development\gitextensions\artifacts\Release\bin -d ArtifactsPublishPath=D:\Development\gitextensions\artifacts\Release\publish\GitExtension
